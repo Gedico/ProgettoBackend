b09b852b2b1808e6e569fc5be60ce0d22e288f7b
Gedico <Gedico3@gmail.com>
2025-12-18 16:14:23 +0100

    commit 18/12

---
+++ src/main/java/ProgettoINSW/backend/dto/inserzionemappa/InserzioneMappaResponse.java
@@ -0,0 +1,13 @@
+package ProgettoINSW.backend.dto.inserzionemappa;
+
+import java.math.BigDecimal;
+
+public class InserzioneMappaResponse {
+
+    private Long idInserzione;
+    private String comune;
+    private BigDecimal latitudine;
+    private BigDecimal longitudine;
+    private BigDecimal prezzo;
+
+}
